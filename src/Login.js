86bfee6291a922e6ddb99f56523b76c2a7b28c09
Devender Bisht <devenderb679@gmail.com>
2022-02-28 23:42:48 +0530

    auth

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+// import { auth } from './firebase'
 import "./login.css"
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+ function HandleEmail(event) {
+     const newValue = event.target.value;
+     setEmail(newValue);
+  }
+
+  function HandlePassword(event) {
+    const newPassValue = event.target.value;
+    setPassword(newPassValue);
+ }
+
+  const login = event => {
+    // console.log("yuvyhvi")
+    event.preventDefault();
+    auth.signInWithEmailAndPassword(email, password)
+    .then((auth) =>{
+      //loggedin
+    })
+    .catch(e => )
+  }
+
+  const register = event =>{
+    event.preventDefault();
+  }
+
   return (
     <div className='login'>
     <Link to= '/'>
@@ -15,15 +43,15 @@ function Login() {
           <h2>Sign-in</h2>
           <form>
               <h5>Email or mobile phone number</h5>
-              <input type='text' />
+              <input type='text' onChange={HandleEmail} value = {email} />
               <h5>Password</h5>
-              <input type='password' />
+              <input type='password' onChange={HandlePassword} value = {password}/>
           </form>
 
-          <button className='login__signInButton'>Sign in</button>
+          <button className='login__signInButton' onClick={login}>Sign in</button>
           <small>Forgot Password?</small>
           <p>By signing, you agree to Amazon's Conditions of Use and Privacy Notice.</p>
-          <button className='login__registerButton'>Create your amazon account</button>
+          <button className='login__registerButton' onClick= {register}>Create your amazon account</button>
        </div>
     </div>
   )
